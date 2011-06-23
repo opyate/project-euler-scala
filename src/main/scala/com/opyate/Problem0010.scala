e09598a9bf023b2e31d2365c50b57e0845f0f026
Juan Uys <opyate@gmail.com>
2011-06-23 13:44:05 +0100

    started on problem 11

--- src/main/scala/com/opyate/Problem0010.scala
+++ src/main/scala/com/opyate/Problem0010.scala
@@ -4,7 +4,7 @@ import tools.primes.InfiniteStreamPrimes
 import tools.Timer
 import Timer._
 
-import com.opyate.tools.db.File._
+
 
 /**
  * Calculate the sum of all the primes below two million.
@@ -16,7 +16,9 @@ object Problem0010 extends App {
 
   def answer = {
     //InfiniteStreamPrimes.getPrimes(2000000).sum
-    sieve.takeWhile(_ < 2000000).sum
+    //sieve.takeWhile(_ < 200000).foldLeft(0)(_+_)
+    var i = BigInt(0)
+    InfiniteStreamPrimes.getPrimes(2000000).foldLeft(i)(_+_)
   }
 
   go
